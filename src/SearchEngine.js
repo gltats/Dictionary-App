6d9b385f8acab22e8bebb7832f78f0757206469a
gltats <74324703+gltats@users.noreply.github.com>
2021-04-04 20:49:10 +0200

    img rendered

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 import './SearchEngine.css';
 import Results from './Results';
+import Photos from './Photos';
 
 export default function SearchEngine(){
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response){
         setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);
     }
 
     function search(event){
@@ -16,6 +22,12 @@ export default function SearchEngine(){
         //documentation: https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "563492ad6f91700001000001ef6b9bfe5a4345fdbd204f578a692536";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+        let headers = {Authorization: `Bearer ${pexelsApiKey}` }
+        
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleKeywordChange(event){
@@ -31,7 +43,8 @@ export default function SearchEngine(){
                  <button className="SearchEngineForm"  type="button" onClick={search}>Search</button>
             </form>
             <div className="exampleWords">i.e. abnegation, anachronistic, ascetic...</div>
-            <Results results={results}/>
+            <Results results={results} />
+            <Photos photos={photos} />
         </div>
         );
 }
